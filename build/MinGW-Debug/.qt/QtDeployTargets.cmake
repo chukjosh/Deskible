fdc09d8e25766116c437d04c9ec9aca9e949028a
chukjosh <chukjosh@gmail.com>
2026-03-29 18:27:23 +0100

    Fix: Debug cmake and ninja project buil error.
    This took a hell out of my time.

--- build/MinGW-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Deskible_FILE C:/Users/chukjosh/Desktop/Projects/QtProjects/Deskible/build/MinGW-Debug/Deskible.exe)
-set(__QT_DEPLOY_TARGET_Deskible_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Deskible_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Svg.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
--- build/MinGW-Debug/CMakeFiles/Deskible.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Deskible.
-# This may be replaced when dependencies are built.
